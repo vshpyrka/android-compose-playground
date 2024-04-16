4b7dcced9bceb454855a7aa2de3d44f860bbc685
Victor Shpyrka <victor.shpyrka@gmail.com>
2024-04-16 08:57:04 +0300

    test: Fix test

--- compose/src/androidTest/java/com/example/compose/ComposeTest.kt
+++ compose/src/androidTest/java/com/example/compose/ComposeTest.kt
@@ -7,11 +7,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
@@ -30,18 +31,53 @@ class ComposeTest {
         }
 
         // Print semantics
-        rule.onRoot().printToLog("AAA")
+        rule.onRoot(useUnmergedTree = true).printToLog("AAA")
 
         rule.onNodeWithText("Hello")
             .assertExists()
         rule.onNode(
             hasText("World")
-            and
-            hasClickAction()
+                    and
+                    hasClickAction()
         ).assertExists()
         rule.onNode(
-            hasTestTag("clickableButton")
+            hasTestTag("clickableButton"),
+            useUnmergedTree = true
         ).assertExists()
+    }
+
+    fun advanceTimeExampleTest() {
+        rule.mainClock.autoAdvance = false
+        rule.setContent {
+            TestComposable()
+        }
+        rule.mainClock.advanceTimeBy(1000)
+        // or go frame by frame
+        rule.mainClock.advanceTimeByFrame()
+
+        // Wait until something appears
+        rule.waitUntil {
+            rule.onAllNodesWithText("Button")
+                .fetchSemanticsNodes()
+                .isNotEmpty()
+        }
+    }
+
+    @Test
+    fun restoreState() {
+        val restorationTester = StateRestorationTester(rule)
+
+        restorationTester.setContent {
+            TestComposable()
+        }
+
+        // Trigger a recreation
+        restorationTester.emulateSavedInstanceStateRestore()
+
+        // Verify next items
+
+        rule.onNodeWithText("Hello")
+            .assertExists()
     }
 }
 
