408bee285ec0f2ed36222497a8848db283554f22
Victor Shpyrka <victor.shpyrka@gmail.com>
2025-03-05 11:30:48 +0200

    chore: Gradle update

--- compose/src/main/java/com/example/compose/CodeLabAdvancedLayoutsActivity.kt
+++ compose/src/main/java/com/example/compose/CodeLabAdvancedLayoutsActivity.kt
@@ -20,19 +20,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.IntrinsicMeasurable
 import androidx.compose.ui.layout.IntrinsicMeasureScope
@@ -41,13 +52,24 @@ import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.layout.MeasureResult
 import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.round
 import com.example.compose.ui.theme.AndroidPlaygroundTheme
+import kotlin.math.max
+import kotlin.math.roundToInt
+import kotlin.text.Typography.ellipsis
 
 class CodeLabAdvancedLayoutsActivity : ComponentActivity() {
 
@@ -56,7 +78,7 @@ class CodeLabAdvancedLayoutsActivity : ComponentActivity() {
 
         setContent {
             AndroidPlaygroundTheme {
-                ImageTest()
+                AdaptiveButtonsEnd()
             }
         }
     }
@@ -205,7 +227,7 @@ private fun ButtonNavItemPreview() {
             text = {
                 Text(text = "Hello")
             },
-            animationProgress =  { 1f }
+            animationProgress = { 1f }
         )
     }
 }
@@ -230,7 +252,7 @@ fun BottomNavItem(
                 content = text
             )
         }
-    ) {measurables, constraints ->
+    ) { measurables, constraints ->
 
         val iconPlaceble = measurables.first { it.layoutId == "icon" }.measure(constraints)
         val textPlaceble = measurables.first { it.layoutId == "text" }.measure(constraints)
@@ -453,4 +475,312 @@ fun Hexagon(position: Int) {
     }
 }
 
+@Composable
+fun AdaptiveButtonsEnd(
+    modifier: Modifier = Modifier
+) {
+    Layout(
+        modifier = modifier,
+        content = {
+            Button(
+                modifier = Modifier.layoutId("primary"),
+                onClick = {}
+            ) {
+                Text("Primary")
+            }
+            Button(
+                modifier = Modifier.layoutId("secondary"),
+                onClick = {}
+            ) {
+                Text("Secondary")
+            }
+        },
+        measurePolicy = { measurables, constraints ->
+            val primaryButtonMeasurable = measurables.find { it.layoutId == "primary" }!!
+            val secondaryButtonMeasurable = measurables.find { it.layoutId == "secondary" }!!
 
+            val primaryButtonMinIntrinsicWidth = primaryButtonMeasurable
+                .minIntrinsicWidth(constraints.maxHeight)
+            val secondaryButtonMinIntrinsicWidth = secondaryButtonMeasurable
+                .minIntrinsicWidth(constraints.maxHeight)
+
+            val showHorizontally = primaryButtonMinIntrinsicWidth <= constraints.maxWidth / 2 &&
+                    secondaryButtonMinIntrinsicWidth <= constraints.maxWidth / 2
+
+            val width = constraints.minWidth
+            val height: Int
+            val primaryButtonPlaceable: Placeable
+            val secondaryButtonPlaceable: Placeable
+
+            if (showHorizontally) {
+                val halfWidthConstraints = constraints.copy(
+                    minWidth = constraints.maxWidth / 2,
+                    maxWidth = constraints.maxWidth / 2,
+                )
+                primaryButtonPlaceable = primaryButtonMeasurable.measure(halfWidthConstraints)
+                secondaryButtonPlaceable = secondaryButtonMeasurable.measure(halfWidthConstraints)
+                height = max(primaryButtonPlaceable.height, secondaryButtonPlaceable.height)
+            } else {
+                val fullWidthConstraints = constraints.copy(
+                    minWidth = constraints.maxWidth
+                )
+                primaryButtonPlaceable = primaryButtonMeasurable.measure(fullWidthConstraints)
+                secondaryButtonPlaceable = secondaryButtonMeasurable.measure(fullWidthConstraints)
+                height = primaryButtonPlaceable.height + secondaryButtonPlaceable.height
+            }
+            layout(width, height) {
+                if (showHorizontally) {
+                    primaryButtonPlaceable.placeRelative(
+                        width / 2,
+                        (height - primaryButtonPlaceable.height) / 2,
+                    )
+                    secondaryButtonPlaceable.placeRelative(
+                        0,
+                        (height - secondaryButtonPlaceable.height) / 2,
+                    )
+                } else {
+                    primaryButtonPlaceable.placeRelative(
+                        0,
+                        0,
+                    )
+                    secondaryButtonPlaceable.placeRelative(
+                        0,
+                        primaryButtonPlaceable.height,
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(widthDp = 200, heightDp = 300)
+@Preview(widthDp = 300, heightDp = 300)
+@Composable
+private fun AdaptiveButtonsEndPreview() {
+    AndroidPlaygroundTheme {
+        Box {
+            AdaptiveButtonsEnd(Modifier.fillMaxWidth())
+        }
+    }
+}
+
+@Preview(widthDp = 200, heightDp = 200)
+@Preview(widthDp = 300, heightDp = 200)
+@Preview(widthDp = 300, heightDp = 400)
+@Preview(widthDp = 300, heightDp = 400, locale = "ar")
+@Composable
+fun TextWidthBadge() {
+    val text =
+        "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    Surface(
+        modifier = Modifier.wrapContentSize()
+    ) {
+        var textLayoutResult: TextLayoutResult? by remember { mutableStateOf(null) }
+        Layout(
+            content = {
+                Text(
+                    modifier = Modifier.layoutId("text"),
+                    text = text,
+                    onTextLayout = {
+                        textLayoutResult = it
+                    }
+                )
+                Icon(
+                    Icons.Default.CheckCircle,
+                    contentDescription = "Received",
+                    modifier = Modifier
+                        .layoutId("badge")
+                        .size(24.dp)
+                )
+            },
+            measurePolicy = { measurables, constraints ->
+                val textPlaceable =
+                    measurables.find { it.layoutId == "text" }!!.measure(constraints)
+                val badgePlaceable =
+                    measurables.find { it.layoutId == "badge" }!!.measure(constraints)
+                val badgeWidth = badgePlaceable.width
+                val badgeHeight = badgePlaceable.height
+
+                val newTextLayoutResult = textLayoutResult!!
+
+                val lastLineEndBoundingBox = newTextLayoutResult.getBoundingBox(
+                    newTextLayoutResult.getLineEnd(
+                        lineIndex = newTextLayoutResult.lineCount - 1,
+                        visibleEnd = true,
+                    ) - 1
+                )
+
+                val lastLineRelativeOffset = IntOffset(
+                    when (layoutDirection) {
+                        LayoutDirection.Ltr -> lastLineEndBoundingBox.right
+                        LayoutDirection.Rtl -> newTextLayoutResult.size.width - lastLineEndBoundingBox.left
+                    }.roundToInt(),
+                    lastLineEndBoundingBox.top.roundToInt(),
+                )
+
+                val displayBadgeInLastLine =
+                    constraints.maxWidth - lastLineRelativeOffset.x >= badgeWidth
+                val width: Int
+                val height: Int
+                val badgeX: Int
+                val badgeY: Int
+
+                if (displayBadgeInLastLine) {
+                    width = max(
+                        newTextLayoutResult.size.width,
+                        lastLineRelativeOffset.x + badgeWidth
+                    )
+                    height = max(
+                        newTextLayoutResult.size.height,
+                        lastLineRelativeOffset.y + badgeHeight
+                    )
+                    badgeX = lastLineRelativeOffset.x
+                    badgeY = lastLineRelativeOffset.y
+                } else {
+                    width = max(newTextLayoutResult.size.width, badgeWidth)
+                    height = newTextLayoutResult.size.height + badgeHeight
+                    badgeX = max(0, newTextLayoutResult.size.width - badgeWidth)
+                    badgeY = newTextLayoutResult.size.height
+                }
+                layout(width, height) {
+                    textPlaceable.placeRelative(0, 0)
+                    badgePlaceable.placeRelative(badgeX, badgeY)
+                }
+            }
+        )
+    }
+}
+
+@Preview(widthDp = 200, heightDp = 300)
+@Preview(widthDp = 300, heightDp = 300)
+@Preview(widthDp = 400, heightDp = 300)
+@Preview(widthDp = 500, heightDp = 300)
+@Preview(widthDp = 600, heightDp = 300)
+@Composable
+fun MoreButtonTextEnd() {
+    val maxLines = 5
+    val text =
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+    val textMeasurer = rememberTextMeasurer()
+    var fullTextLayoutResult: TextLayoutResult? by remember { mutableStateOf(null) }
+    var showButton by remember { mutableStateOf(false) }
+
+    var ellipsizedLayoutResult: TextLayoutResult? by remember { mutableStateOf(null) }
+
+    Surface(
+        modifier = Modifier.wrapContentSize()
+    ) {
+        val contentColor = LocalContentColor.current
+        val style = LocalTextStyle.current
+
+        Layout(
+            content =  {
+                Text(
+                    modifier = Modifier
+                        .layoutId("text")
+                        .drawBehind {
+                            drawText(
+                                if (showButton) {
+                                    ellipsizedLayoutResult!!
+                                } else {
+                                    fullTextLayoutResult!!
+                                },
+                                contentColor,
+                            )
+                        },
+                    text = text,
+                    style = style,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = maxLines,
+                    onTextLayout = {
+                        fullTextLayoutResult = it
+                    },
+                    // Text will be rendered in the modifier, but keep the full text for semantics
+                    color = Color.Transparent
+                )
+
+                Button(
+                    modifier = Modifier.layoutId("button"),
+                    onClick = {},
+                ) {
+                    Text("More")
+                }
+            },
+            measurePolicy = { measurable, constraints ->
+                val textMeasurable = measurable.find { it.layoutId == "text" }
+                val textPlaceable = textMeasurable!!.measure(constraints)
+
+                // The full text layout is now available from the above measure
+                val newFullTextLayoutResult = fullTextLayoutResult!!
+
+                val buttonMeasurable = measurable.find { it.layoutId == "button" }
+                val buttonPlaceable = buttonMeasurable!!.measure(constraints)
+
+                // We need to show the button if the last line was ellipsized
+                showButton = newFullTextLayoutResult.isLineEllipsized(newFullTextLayoutResult.lineCount - 1)
+
+                if (showButton) {
+                    val ellipsisTextLayoutResult = textMeasurer.measure(
+                        text = ellipsis.toString(),
+                        style = style,
+                    )
+
+                    // Determine how much space is available for the last line of text without
+                    // the ellipsis
+                    val lastLineTextWidth = constraints.maxWidth -
+                            buttonPlaceable.width -
+                            ellipsisTextLayoutResult.size.width
+                    val lastCharOffset = newFullTextLayoutResult.getOffsetForPosition(
+                        Offset(
+                            lastLineTextWidth.toFloat(),
+                            newFullTextLayoutResult.getLineBottom(maxLines - 1)
+                        )
+                    )
+
+                    val newEllipsizedLayoutResult = textMeasurer.measure(
+                        text.substring(0, lastCharOffset) + ellipsis,
+                        style = style,
+                        constraints = constraints,
+                    )
+                    ellipsizedLayoutResult = newEllipsizedLayoutResult
+
+                    val lastLineEndBoundingBox = newEllipsizedLayoutResult.getBoundingBox(
+                        newEllipsizedLayoutResult.getLineEnd(
+                            newEllipsizedLayoutResult.lineCount - 1,
+                            visibleEnd = true,
+                        ) - 1
+                    )
+
+                    val lastLineRelativeOffset = Offset(
+                        when (layoutDirection) {
+                            LayoutDirection.Ltr -> lastLineEndBoundingBox.right
+                            LayoutDirection.Rtl -> newEllipsizedLayoutResult.size.width - lastLineEndBoundingBox.left
+                        },
+                        lastLineEndBoundingBox.top
+                    ).round()
+
+                    val width = constraints.maxWidth
+                    val height = max(
+                        newFullTextLayoutResult.size.height,
+                        lastLineRelativeOffset.y + buttonPlaceable.height
+                    )
+                    val buttonX = constraints.maxWidth - buttonPlaceable.width
+                    val buttonY = lastLineRelativeOffset.y
+
+                    layout(width, height) {
+                        textPlaceable.placeRelative(0, 0)
+                        buttonPlaceable.placeRelative(buttonX, buttonY)
+                    }
+                } else {
+                    layout(
+                        width = newFullTextLayoutResult.size.width,
+                        height = newFullTextLayoutResult.size.height
+                    ) {
+                        textPlaceable.placeRelative(0, 0)
+                    }
+                }
+            }
+        )
+    }
+}
