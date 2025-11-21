bdfd9bd17296b7a0740dd6dd0e4b9039b9ed3233
Victor Shpyrka <victor.shpyrka@gmail.com>
2025-11-21 11:13:36 +0200

    chore: Build fix

--- compose/src/main/java/com/example/compose/ComposeLauncherActivity.kt
+++ compose/src/main/java/com/example/compose/ComposeLauncherActivity.kt
@@ -44,7 +44,6 @@ class ComposeLauncherActivity : ComponentActivity() {
             "Shared Element Transition" to SharedElementTransitionActivity::class,
             "Accessibility" to ComposeAccessibilityActivity::class,
             "Touch And Input" to TouchInputActivity::class,
-            "Material Expressive" to ComposeExpressiveActivity::class,
         )
         setContent {
             Scaffold { paddingValues ->
